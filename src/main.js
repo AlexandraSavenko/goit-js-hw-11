c258b0da2d70ccaaa639400b2c4fec3f7b674fbb
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-07-06 14:34:04 +0300

    some progress with styles

--- src/main.js
+++ src/main.js
@@ -32,33 +32,36 @@ function getPicturesByQuery(query) {
 
 function createItem(items) {
   gallery.innerHTML = '';
-  items.forEach(item => {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery-item');
-    galleryItem.innerHTML = `<a href="${item.largeImageURL}" class="gallery-link">
+  const myGallery = items
+    .map(
+      item =>
+        `<li class="gallery-item">
+      <a href="${item.largeImageURL}" class="gallery-link">
     <img src="${item.webformatURL}" alt="${item.tags}" class="gallery-image" />
   </a> 
-  <div class="item-info>
-<div class="">
+  <div class="item-info">
+<div class="item-info-box">
   <p class="likes name">Likes</p>
   <p class="likes value">${item.likes}</p>
 </div>
-<div class="">
+<div class="item-info-box">
   <p class="views name">Views</p>
   <p class="views value">${item.views}</p>
 </div>
-<div class="">
+<div class="item-info-box">
   <p class="com name">Comments</p>
   <p class="com value">${item.comments}</p>
 </div>
-<div class="">
+<div class="item-info-box">
   <p class="downl name">Downloads</p>
   <p class="downl value">${item.downloads}</p>
 </div>
-  </div>
-`;
-    gallery.appendChild(galleryItem);
-  });
+</div>
+  </li>
+`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', myGallery);
   let lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
